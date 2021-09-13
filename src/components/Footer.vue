0fdb588bb1c0a417eaf29716927e7b377df4ae62
David <raymondinf23@gmail.com>
2021-09-13 02:46:22 -0400

    Create Footer component

---
+++ src/components/Footer.vue
@@ -0,0 +1,12 @@
+<template>
+  <footer class="attribution">
+    Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">Frontend Mentor</a>. 
+    Coded by <a href="https://github.com/MrDavid0614" target="_blank">David Mora</a>.
+  </footer>
+</template>
+
+<style scoped>
+footer {
+  text-align: center;
+}
+</style>
